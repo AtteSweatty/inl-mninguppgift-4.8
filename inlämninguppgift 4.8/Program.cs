c61e8b6adb8f85ba77cfa43028d76c2fb20ccfbc
arha06001 <arha06001@E-PW0234JA>
2023-10-20 12:55:20 +0200

    Add project files.

---
+++ inlämninguppgift 4.8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ange tringelns sidlängd");
+        int sidlängd = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+            
+        for (int i = 0; i < sidlängd; i++)
+        {
+            for (int j = 0; j < i + 1; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
